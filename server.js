147ee7f914f36f159d1b61c89334d4002e0983ab
Gaston Kennedy <gastontk@hotmail.com>
2017-05-02 14:53:04 -0700

    Changing listen port

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.use(express.static(path.join(__dirname, './node_modules')));
 
 
 
-app.listen(3000, function(){
-	console.log('Listening on port 3000');
+app.listen(8000, function(){
+	console.log('Listening on port 8000');
 })
